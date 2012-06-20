39de8cfcb4ba190cb5834b76e47cdebc87aa0807
Andrew Montgomery-Hurrell <andrew@multiplay.co.uk>
2012-06-20 17:14:15 +0100

    Initial commit of code so far - still unfinished.

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 chaperone.js
 ============
 
-A jQuery plugin for doing simple guided tours of websites
+A jQuery plugin for doing simple guided tours of websites.
+
+Inspired by [Joyride](http://www.zurb.com/playground/jquery-joyride-feature-tour-plugin)
---
+++ jquery.chaperone.js
@@ -0,0 +1,57 @@
+(function($, undefined) {
+	// # Chaperone.js
+	//
+	// A jQuery plugin for doing simple guided tours of websites
+	$.fn.chaperone = function(options) {
+		options = $.extend(options, $.fn.chaperone.defaults);
+
+		return this.each(function(){
+			var elem = $(this),
+				body = $('body'),
+				opts = $.extend(options, elem.data());
+
+			if(elem.data('chaperone')) {
+				container = elem.data('chaperone');
+			} else {
+				container = $('<div class="chaperone-steps"></div>');
+				elem.data('chaperone', container);
+				body.append(container);
+			}
+
+			elem.find(opts.step).each(function(index, step) {
+				var code = $(opts.template),
+					title = step.attr('data-title'),
+					content = step.html();
+				if(title) {
+					code.find('.title').html(title);
+				} else {
+					code.find('.title').remove();
+				}
+				code.find('.content').html(content);
+				code.hide();
+				container.append(code);
+			});
+		});
+	};
+
+	$.fn.chaperone.defaults = {
+		step: 'li',
+		animate: true,
+		template: [
+			'<div class="chaperone">',
+				'<a class="close">&times;</a>',
+				'<div class="content-wrapper">',
+					'<h4 class="title">Title</h4>',
+					'<div class="content">Content</div>',
+				'</div>',
+			'</div>'
+		].join(),
+		keyboard: true,
+		nextKey: [39, 40, 13, 32],	// Right arrow | Up arrow | Enter | Space
+		prevKey: [37, 38, 8],		// Left arrow | Down arrow | Backspace
+		closeKey: 27,				// Escape
+		keepInside: true,
+		keepInsideBoundary: document
+	};
+
+})(jQuery);
